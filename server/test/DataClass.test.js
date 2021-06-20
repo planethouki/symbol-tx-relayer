0f4d0fb3e5a1203d9ad8c52961a704535da2881c
planet★箒星 <26595148+planethouki@users.noreply.github.com>
2021-06-20 23:21:19 +0900

    del duplicate

--- server/test/DataClass.test.js
+++ server/test/DataClass.test.js
@@ -22,60 +22,16 @@ const {
     epochAdjustment,
     mosaicId,
     privateKey,
-    generationHash
+    generationHash,
+    restUrl,
+    signature,
+    publicKey,
+    parentHash,
+    signedTransaction,
+    cosignatureSignedTransaction
 } = require('./testEnv');
 
-let signature, publicKey, hash, signedTransaction, cosignatureSignedTransaction;
-
 describe('DataClass', () => {
-
-    before(() => {
-        const serverAccount = Account.createFromPrivateKey(
-            privateKey,
-            networkType
-        );
-        const userAccount = Account.createFromPrivateKey(
-            'C604F0AE90295D7311F9F78EBDC2D91D22861CC843E58266221DB06F784D1FCB',
-            networkType
-        );
-        const transferTransaction = TransferTransaction.create(
-            Deadline.create(epochAdjustment),
-            Account.generateNewAccount(networkType).address,
-            [new Mosaic(new MosaicId(mosaicId), UInt64.fromUint(1))],
-            PlainMessage.create(''),
-            networkType
-        );
-        const dummyTransaction = TransferTransaction.create(
-            Deadline.create(epochAdjustment),
-            Account.generateNewAccount(networkType).address,
-            [new Mosaic(new MosaicId(mosaicId), UInt64.fromUint(0))],
-            PlainMessage.create(''),
-            networkType
-        );
-        const aggregateTransaction = AggregateTransaction.createComplete(
-            Deadline.create(epochAdjustment),
-            [
-                transferTransaction.toAggregate(userAccount.publicAccount),
-                dummyTransaction.toAggregate(serverAccount.publicAccount)
-            ],
-            networkType,
-            [],
-            UInt64.fromUint(50000)
-        );
-        signedTransaction = serverAccount.signTransactionWithCosignatories(
-            aggregateTransaction,
-            [],
-            generationHash
-        );
-        cosignatureSignedTransaction = CosignatureTransaction.signTransactionPayload(
-            userAccount,
-            signedTransaction.payload,
-            generationHash
-        );
-        signature = cosignatureSignedTransaction.signature;
-        publicKey = cosignatureSignedTransaction.signerPublicKey;
-        hash = cosignatureSignedTransaction.parentHash;
-    });
 
     it('instance', (done) => {
         let data;
@@ -105,7 +61,7 @@ describe('DataClass', () => {
                 data.save(signedTransaction, callback)
             },
             (callback) => {
-                data.load(hash, callback);
+                data.load(parentHash, callback);
             },
             (signedTransaction, callback) => {
                 expect(signedTransaction).to.be.an.instanceOf(SignedTransaction);
@@ -152,7 +108,7 @@ describe('DataClass', () => {
                 data.initialize(callback);
             },
             async.reflect((callback) => {
-                data.load(hash, callback);
+                data.load(parentHash, callback);
             }),
             (signedTransaction, callback) => {
                 expect(signedTransaction).not.to.be.instanceof(SignedTransaction);
